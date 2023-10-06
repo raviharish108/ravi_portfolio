50bf5c2f343c95aebe8008d9ef8a22391bea8080
RaviHarish <raviharish108@gmail.com>
2023-10-06 13:36:22 +0530

    resume uploades

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -19,7 +19,7 @@ export function About(){
               <div  className="row about_p ">
                 <div className=" col-md-6 about_p ">
                     <p className="about_p fw-normal para">I am Ravi, web App developer from Ramanathapuram, India. I have good experience in App  building and customization.</p>
-                    <button type="button" class="btn btn-danger"><a href="https://drive.google.com/file/d/1nuhzfpL6QfJODWtNrtKJEFnvvc9ea72o/view?usp=sharing" target="blank" className="bg-danger fw-bold ">Download CV</a></button>
+                    <button type="button" class="btn btn-danger"><a href="https://drive.google.com/file/d/18SVyqXIqXiBx0kQboJ11ntbCZTAqlTNZ/view?usp=sharing" target="blank" className="bg-danger fw-bold ">Download CV</a></button>
                 </div>
                 <div className=" col-md-6 about_p fw-bold ">
                     <p className="about_p bars_right">Development</p>
