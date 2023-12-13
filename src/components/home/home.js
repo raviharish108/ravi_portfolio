195b7f6e5773dfacbcb2a82c15cee5eb8b5d5e3b
RaviHarish <raviharish108@gmail.com>
2023-12-13 08:24:20 +0530

    update resume

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -14,7 +14,7 @@ export function Home(){
                 <li ><a href="https://www.linkedin.com/in/ravindran-ravi-280a60241/" target="blank"><i class="bi bi-linkedin icon" style={{ fontSize: 25 }}></i></a></li>
                 <li ><a href="https://github.com/raviharish108/" target="blank"><i class="bi bi-bi bi-github icon" style={{ fontSize: 25 }}></i></a></li>
             </ul>
-              <Button variant="danger"><a href="https://drive.google.com/file/d/1Yxmj9t-hyiccD8OEQ_McU_rILKcnZF48/view?usp=sharing" target="blank" className="bg-danger fw-bold">Hire Me</a></Button>
+              <Button variant="danger"><a href="https://drive.google.com/file/d/1Yxmj9t-hyiccD8OEQ_McU_rILKcnZF48/view?pli=1" target="blank" className="bg-danger fw-bold">Hire Me</a></Button>
               
         </div> 
     )
